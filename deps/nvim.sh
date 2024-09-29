f3173de7fb9fb6f954ef755468bb6b05254f7a5b
Dima Lampsi <dlampsi@gmail.com>
2024-09-29 09:40:50 +0300

    Adding nvim config directory creation

--- deps/nvim.sh
+++ deps/nvim.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
+# Create nvim config directory since it doesn't exist by default.
+mkdir -p $HOME/.config/nvim
+
 # Install vim-plug - https://github.com/junegunn/vim-plug
 # NOTE: Run ':PlugInstall' in nvim after this.
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-
